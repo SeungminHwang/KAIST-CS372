ddee1282820b743a382ebb9468cdce81eb920cf5
Seungmin Hwang <hwangtmdals@kaist.ac.kr>
2021-05-30 23:48:49 +0900

    small updates

--- src/dev/korSentTokenizer.py
+++ src/dev/korSentTokenizer.py
@@ -1,5 +1,8 @@
 from khaiii import KhaiiiApi
 api = KhaiiiApi()
 
-for word in api.analyze('안녕, 세상.'):
+sent = "(버전 1.0) 한국어 예문 문법성(수용성)을 언어 사용자가 평가한 정보가 포함된 말뭉치입니다."
+
+
+for word in api.analyze(sent):
     print(word)
--- src/dev/tsvReader.py
+++ src/dev/tsvReader.py
@@ -9,18 +9,57 @@ f = open(filename, 'r', encoding='utf-8')
 reader = csv.reader(f, delimiter='\t')
 
 
-sents = []
+sents_true = []
+sents_false = []
 for line in reader:
-    #print(line[3])
-    sent = line[3]
-    sents.append(sent)
+    if(line[1] == '1'):
+        sent = line[3]
+        sents_true.append(sent)
+    if(line[1] == '0'): # wrong sentence
+        sent = line[3]
+        sents_false.append(sent)
 
 f.close()
 
-f = open("output.txt", 'w')
-for sent in sents:
-    f.write("--------------------\n")
-    for word in api.analyze(sent):
-        f.write(str(word) + '\n')
 
+# sent + tag (for tag tree) - correct
+f = open("train_sent_correct.txt", 'w')
+for sent in sents_true:
+    tag_seq = []
+    for word in api.analyze(sent):
+        word_str = str(word)
+        raw_word, tagged_words_raw = word_str.split('\t')
+        tagged_words = tagged_words_raw.split('+')
+        tags = [tw.split('/')[1] for tw in tagged_words]
+        tag_seq += tags
+    
+    tag_seq_str = ""
+    for tag in tag_seq:
+        if(tag[-1] == ' '):
+            tag = tag[:-1]
+        tag_seq_str += (tag + ',')
+    output_str = sent + ';' + tag_seq_str
+    #print(output_str)
+    f.write(output_str + '\n')
+f.close()
+
+# sent + tag (for tag tree) - incorrect
+f = open("train_sent_incorrect.txt", 'w')
+for sent in sents_false:
+    tag_seq = []
+    for word in api.analyze(sent):
+        word_str = str(word)
+        raw_word, tagged_words_raw = word_str.split('\t')
+        tagged_words = tagged_words_raw.split('+')
+        tags = [tw.split('/')[1] for tw in tagged_words]
+        tag_seq += tags
+    
+    tag_seq_str = ""
+    for tag in tag_seq:
+        if(tag[-1] == ' '):
+            tag = tag[:-1]
+        tag_seq_str += (tag + ',')
+    output_str = sent + ';' + tag_seq_str
+    #print(output_str)
+    f.write(output_str + '\n')
 f.close()
